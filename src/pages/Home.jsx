c52c0d1e8b51049771e2eb5477ad10bc70c31215
Aarushi <72608824+aarushi353@users.noreply.github.com>
2023-03-04 09:13:39 +0530

    client

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
 import React,{ useState, useEffect } from 'react'
 import Loader from "../components/Loader";
 import FieldForm from "../components/FieldForm";
+import Card from '../components/Card';
+const RenderCards = ({ data, title }) => {
+  if(data?.length > 0) {
+    return data.map((post) => <Card key={post._id} {...post} />)
+  }
+
+  return (
+    <h2 className="mt-5 font-bold text-[#22D3EE] text-xl uppercase">{title}</h2>
+  )
+}
+
 const Home = () => {
-  const [loading, setLoading] = useState(true);
-  // const [allPosts, setAllPosts] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+  const [searchText, setSearchText] = useState('');
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -22,7 +34,22 @@ const Home = () => {
           </div>
         ):(
        <>
-       
+        {searchText && (
+          <h2 className='="font-medium text-[#666e75] text-xl mb-3'>Showing results for <span className="text-[#222328]">{searchText}</span></h2>
+        )}
+        <div classNamw="grid lg:grids-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+        {searchText ? (
+                <RenderCards
+                  data={searchedResults}
+                  title="No Search Results Found"
+                />
+              ) : (
+                <RenderCards
+                  data={allPosts}
+                  title="No Posts Yet"
+                />
+              )}
+        </div>
        </>
        )}
       </div>

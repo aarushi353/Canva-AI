a3fb0ecaec5805257d76826063d53de296315f65
Aarushi <72608824+aarushi353@users.noreply.github.com>
2023-03-04 10:30:21 +0530

    routes added

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './mongodb/connect.js';
+import apiRoutes from './routes/apiRoutes.js';
+import  postRoutes  from './routes/postRoutes.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit:'50mb' }));
+
+app.use('/api/v1/post',postRoutes);
+app.use('/api/v1/dalle',apiRoutes);
+app.get('/', async(req, res) => {
+    res.send('Hello World!');
+})
+
+const startServer = async () => {
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080,()=>console.log('Server has started on port http://localhost:8080'))
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
+
+startServer();
